279345b5b6facaffbc7e9fb1488f07e14080387d
Sergei Gushchin <guschins@gmail.com>
2023-12-12 22:52:11 +0100

    change url

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'POST' && req.url === '/addData') {
+    if (req.method === 'POST' && req.url === '') {
         let body = '';
 
         req.on('data', (chunk) => {
