6d70043bc1b88cee7f056462f17b81cd8368b4b7
Sergei Gushchin <guschins@gmail.com>
2023-12-14 14:12:10 +0100

    add get

--- server.js
+++ server.js
@@ -41,6 +41,17 @@ const server = http.createServer((req, res) => {
                 return;
             }
         });
+    } else if (req.method === 'GET' && req.url === '/getData') {
+        fs.readFile('data.json', 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Internal Server Error' }));
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
+            res.end(data);
+        });
     } else if (req.method === 'GET' && req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end('<h1>Server is running successfully!</h1>');
